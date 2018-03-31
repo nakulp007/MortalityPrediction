b98647c5fd262e619abdec5349483cd606f50ff5
Nakul Patel <npatel72@gatech.edu>
2018-03-31 13:40:34 -0400

    Load RDD Raw Data

--- src/main/scala/edu/gatech/cse8803/model/models.scala
+++ src/main/scala/edu/gatech/cse8803/model/models.scala
@@ -4,29 +4,45 @@
 
 package edu.gatech.cse8803.model
 
-case class LabResult(patientID: String, date: Long, labName: String, value: String)
+import java.sql.Date
 
-case class Diagnostic(patientID: String, date: Long, icd9code: String, sequence: Int)
 
-case class Medication(patientID: String, date: Long, medicine: String)
 
-abstract class VertexProperty
 
-case class PatientProperty(patientID: String, sex: String, dob: String, dod: String) extends VertexProperty
+case class PatientId(patientID: String)
 
-case class LabResultProperty(testName: String) extends VertexProperty
+case class Patient(patientID: String, isMale: Boolean, dob: Date, isDead: Double, dod: Date, indexDate: Date, age: Double)
 
-case class DiagnosticProperty(icd9code: String) extends VertexProperty
 
-case class MedicationProperty(medicine: String) extends VertexProperty
 
-abstract class EdgeProperty
 
-case class SampleEdgeProperty(name: String = "Sample") extends EdgeProperty
+case class Chart(patientID: String, lastChartDate: Date)
 
-case class PatientLabEdgeProperty(labResult: LabResult) extends EdgeProperty
+case class LabResult(patientID: String, hadmID: String, date: Date, labName: String, value: Double)
 
-case class PatientDiagnosticEdgeProperty(diagnostic: Diagnostic) extends EdgeProperty
+case class Diagnostic(patientID: String, hadmID: String, date: Date, icd9code: String, sequence: Int)
 
-case class PatientMedicationEdgeProperty(medication: Medication) extends EdgeProperty
+case class Medication(patientID: String, hadmID: String, date: Date, medicine: String)
 
+
+
+
+case class Note(patientID: String, hadmID: String, chartDate: Date, category: String, description: String, text: String)
+
+
+
+
+
+case class IcuStays(patientID: String, inDate: Date, outDate: Date)
+
+case class Saps2(patientID: String, hadmID: String, icuStayID: String, sapsScore: Double, scoreProbability: Double, ageScore: Double,
+                 hrScore: Double, sysbpScore: Double, tempScore: Double, pao2fio2Score: Double, uoScore: Double,
+                 bunScore: Double, wbcScore: Double, potassiumScore: Double, sodiumScore: Double,
+                 bicarbonateScore: Double, bilirubinScore: Double, gcsScore: Double, comorbidityScore:
+                 Double, admissiontypeScore: Double)
+
+				 
+//need to figure out 
+//class can not have that many params
+case class Comorbidities(patientID: String, hadmID: String)
+						 
